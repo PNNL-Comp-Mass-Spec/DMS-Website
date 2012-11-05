9b6a47ae13dfb818b167e42fd86be25c2b229b6e
grkiebel <grkiebel@pnnl.gov>
2012-11-05 10:43:37 -0800

    OSM package item delete

--- application/views/cmd/osm_package_cmds.php
+++ application/views/cmd/osm_package_cmds.php
@@ -1,1 +1,40 @@
-Blarg!
+<script type="text/javascript">
+function performOperation(mode) {
+	var list = '';
+	var rows = document.getElementsByName('ckbx');
+	for (var i = 0; i < rows.length; i++) {
+		if ( rows[i].checked )
+			list  += rows[i].value;
+	}
+	if(list=='') {
+		alert('You must select items'); 
+		return;
+	}
+	if ( !confirm("Are you sure that you want to update the database?") )
+		return;
+
+	var url =  "<?= site_url().'osm_package_items/operation/' ?>";
+	$('paramListXML').value = list;
+	$('entry_cmd_mode').value = mode;
+	var p = $('operation_form').serialize(true);
+	submitOperation(url, p);
+}
+
+</script>
+
+
+<div class='LRCmds'>
+<?php $this->load->view("main/list_report_cmd_reporting"); ?>
+
+<form name="DBG" id="operation_form" action="">
+
+<input type="hidden" id="entry_cmd_mode" name="command" value="" />
+<input type='hidden' id='paramListXML' name='paramListXML' />
+<input type="hidden" id="comment" name="comment" value="" />
+
+<div>
+<input class="lst_cmd_btn" type="button" value="Delete Selected Items From Package" onClick='performOperation("delete")' title='Remove the selected items from their OSM package'/>
+</div>
+
+</form>
+</div>
