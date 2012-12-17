81026041c720175bd007a4c5c152f5f0f5669636
grkiebel <grkiebel@pnnl.gov>
2012-12-17 13:23:45 -0800

    Modify resource links

--- application/views/resource_links/base2.php
+++ application/views/resource_links/base2.php
@@ -1,8 +1,12 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url().'css/base.css' ?>" />
 <link rel="stylesheet" type="text/css" href="<?= base_url().'css/nav_bar.css' ?>" />
 
-<script type="text/javascript" src="<?= base_url().'javascript/prototype.js' ?>"></script>
-<script type="text/javascript" src="<?= base_url().'scriptaculous/scriptaculous.js' ?>"></script>
+<link rel="stylesheet" type="text/css" href="<?= base_url().'css/jquery-ui-1.8.21.custom.css' ?>" />
+
+<script type="text/javascript" src="<?= base_url().'javascript/jquery-1.7.2.min.js' ?>"></script>
+<script type="text/javascript" src="<?= base_url().'javascript/jquery-ui-1.8.21.custom.min.js' ?>"></script>
+<script type="text/javascript" src="<?= base_url().'javascript/jquery.event.drag-2.0.min.js' ?>"></script>
+<script type="text/javascript" src="<?= base_url().'javascript/jquery.unobtrusive-ajax.min.js' ?>"></script>
 
 <script type="text/javascript" src="<?= base_url().'javascript/dms2.js' ?>"></script>
 
