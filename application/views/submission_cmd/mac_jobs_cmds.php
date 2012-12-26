8660de76ba919b6e6242d53f37f2965c0246d4a8
grkiebel <grkiebel@pnnl.gov>
2012-12-26 11:18:52 -0800

    JQuery: Misc

--- application/views/submission_cmd/mac_jobs_cmds.php
+++ application/views/submission_cmd/mac_jobs_cmds.php
@@ -2,9 +2,11 @@
 
 // get supplemental form fields via an AJAX call
 function load_param_form() {
-	var url = gamma.pageContext.site_url + gamma.pageContext.my_tag + '/parameter_form/' + $('#scriptName').val();
+	var script = $('#scriptName').val();
+	var url = gamma.pageContext.site_url + gamma.pageContext.my_tag + '/parameter_form/' + script;
 	epsilon.load_suplemental_form(url, {}, 'param_container', function() {
 		set_param_row_visibility("hide_input", "none");
+		if(script) $('#cmd_buttons').show();
 	});
 }
 function choose_template(template_name) {
@@ -18,11 +20,17 @@ function set_param_row_visibility(class_name, visibility) {
 	});
 }
 
-//try and set up the supplemental form when the page loads
 $(document).ready(function () { 
+	// relocate standard family command buttons
+	$('#relocated_buttons').append($('#cmd_buttons'));
+	$('#cmd_buttons').hide();
+	
+	// define action to capture contents of param form
+	// as xml copied to main form field
 	epsilon.actions.before = function() {
 		epsilon.copy_param_form_to_xml_param_field('param_form', 'jobParam');
 	}
+	
 	load_param_form();
 });	
 </script>
@@ -32,3 +40,4 @@ $(document).ready(function () {
 <!-- supplemental form fields load here via AJAX -->
 </div>
 </form>
+<div id='relocated_buttons'></div>
