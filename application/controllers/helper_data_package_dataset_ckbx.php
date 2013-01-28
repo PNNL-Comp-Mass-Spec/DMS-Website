0d135c634be910c9d12e5fe4c5eb9a70dd46af6f
grkiebel <grkiebel@pnnl.gov>
2013-01-28 14:32:02 -0800

    General testing and fixing

---
+++ application/controllers/helper_data_package_dataset_ckbx.php
@@ -0,0 +1,16 @@
+<?php
+require("base_controller.php");
+
+class helper_data_package_dataset_ckbx extends Base_controller {
+
+	// --------------------------------------------------------------------
+	function __construct()
+	{
+		// Call the parent constructor
+		parent::__construct();
+
+		$this->my_tag = "helper_data_package_dataset_ckbx";
+		$this->my_title = "Data Package Datasets";
+	}
+}
+?>
