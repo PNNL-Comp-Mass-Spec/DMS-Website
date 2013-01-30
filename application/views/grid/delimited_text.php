98b6e44be8c2df7ba194fced2dd69dc4c0109af4
grkiebel <grkiebel@pnnl.gov>
2013-01-30 11:53:46 -0800

    General testing and fixing

--- application/views/grid/delimited_text.php
+++ application/views/grid/delimited_text.php
@@ -5,13 +5,13 @@ Import/Export Delimited Text <a id="delimited_text_panel_btn" href="javascript:v
 <div id='delimited_text_panel' class='ctl_panel'>
 <div class='ctl_panel'>
 <span class='ctls'>
-	<a id='import_grid_btn' href='javascript:void(0)' >Import</a> grid contents from delimited text
+	<a id='import_grid_btn' href='javascript:void(0)' title='Replace current contents of grid from delimited text' >Import</a> grid contents from delimited text
 </span>
 <span class='ctls'>
-	<a id='update_grid_btn' href='javascript:void(0)' >Update</a> grid contents from delimited text
+	<a id='update_grid_btn' href='javascript:void(0)' title='Update current contents of grid from delimited text' >Update</a> grid contents from delimited text
 </span>
 <span class='ctls'>
-	<a id='export_grid_btn' href='javascript:void(0)' >Export</a> grid contents to delimited text
+	<a id='export_grid_btn' href='javascript:void(0)' title='Copy current contents of grid to delimited text'>Export</a> grid contents to delimited text
 </span>
 </div>
 
