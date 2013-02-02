f79d6e665e7fb85b562a45541b240b35d4c6aae6
grkiebel <grkiebel@pnnl.gov>
2013-02-02 10:55:20 -0800

    HTML5 doctype

--- application/views/top_level_frames.php
+++ application/views/top_level_frames.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 
 <!-- Pacific Northwest National Laboratory, Richland, WA -->
 <!-- Copyright 2005, Battelle Memorial Institute         -->
