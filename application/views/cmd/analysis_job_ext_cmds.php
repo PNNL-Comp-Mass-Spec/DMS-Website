88a5a011b1562a75d0a5e2dff5c066c84c3593b7
grkiebel <grkiebel@pnnl.gov>
2013-02-03 07:40:10 -0800

    Removed unused files

--- application/views/cmd/analysis_job_ext_cmds.php
+++
@@ -1,8 +0,0 @@
-<div class='LRCmds'>
-<h3>Commands</h3>
-<form name="DBG" id="cmds" >
-<ul>
-<a href='javascript:lambda.transferSelectedListData("analysis_job")'><img src='<?= base_url() ?>images/btn.png' border='0' ></a> Transfer Data
-</ul>
-</form>
-</div>
