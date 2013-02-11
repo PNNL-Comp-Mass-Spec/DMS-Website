ab012df402f8e4ed01e490600c4595e8a8ff0a6f
Ken Auberry <kenneth.auberry@pnnl.gov>
2013-02-11 13:00:43 -0800

    DMS Refactor Website: Added webfont loading code to views/resource_links/base2css.php

--- application/views/resource_links/base2css.php
+++ application/views/resource_links/base2css.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url().'css/nav_bar.css' ?>" />
 <link rel="stylesheet" type="text/css" href="<?= base_url().'css/themes/cupertino/jquery-ui-1.10.0.custom.css' ?>" />
 <link rel="stylesheet" type="text/css" href="<?= base_url().'chosen/chosen/chosen.css' ?>" />
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600|Open+Sans:400,600' rel='stylesheet' type='text/css'>
