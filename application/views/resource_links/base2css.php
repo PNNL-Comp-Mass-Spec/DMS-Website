44e1a149bb084421fed29990b36f4b0ba3249a79
D3L243 <D3L243@Monroe3.pnl.gov>
2013-02-13 13:56:38 -0800

    Now referencing local copies of Open Sans and Source Sans Pro fonts

--- application/views/resource_links/base2css.php
+++ application/views/resource_links/base2css.php
@@ -3,4 +3,6 @@
 <link rel="stylesheet" type="text/css" href="<?= base_url().'css/nav_bar.css' ?>" />
 <link rel="stylesheet" type="text/css" href="<?= base_url().'css/themes/cupertino/jquery-ui-1.10.0.custom.css' ?>" />
 <link rel="stylesheet" type="text/css" href="<?= base_url().'chosen/chosen/chosen.css' ?>" />
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600|Open+Sans:400,600' rel='stylesheet' type='text/css'>
+<!--<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600|Open+Sans:400,600' rel='stylesheet' type='text/css'>-->
+<link rel="stylesheet" type="text/css" href="<?= base_url().'resources/fonts/opensans/opensans.css' ?>" />
+<link rel="stylesheet" type="text/css" href="<?= base_url().'resources/fonts/sourcesanspro/sourcesanspro.css' ?>" />
