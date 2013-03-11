8e8a1422378682e758057bcdc85c2b08de2c6c3e
grkiebel <grkiebel@pnnl.gov>
2013-03-11 09:56:19 -0700

    Added qunit infrastructure for JS unit testing

---
+++ application/views/unit_tests/test_frame.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title><?= $title ?></title>
+	<link rel="stylesheet" href="<?= base_url() ?>qunit/qunit-1.11.0.css">
+	<? $this->load->view('resource_links/base2css') ?>
+</head>
+<body>
+	<div id="qunit"></div>
+	<div id="qunit-fixture"></div>
+
+<? $this->load->view('resource_links/base2js') ?>
+
+	<script src="<?= base_url() ?>qunit/qunit-1.11.0.js"></script>
+	<script src="<?= base_url() ?>javascript/unit_tests/<?= $testFile ?>.js"></script>
+</body>
+</html>
---
+++ javascript/unit_tests/gamma_test.js
@@ -0,0 +1,13 @@
+test( "gamma trim", function() {
+  equal( gamma.trim(" Passed! "), "Passed!" );
+});
+
+test("gamma remove", function() {
+	expect(1);
+	var target = ['one', 'two', 'three', 'four', 'five'];
+	var remove = ['two', 'four'];
+	var expected = ['one', 'three', 'five'];
+	var result = gamma.removeItems(target, remove);
+	deepEqual( result, expected, "Array items successfully removed");
+
+});
