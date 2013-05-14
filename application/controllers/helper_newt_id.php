e2b937ab60cabc390d6e1441d1caedfb2d9b11e3
matthew monroe <matthew.monroe@pnnl.gov>
2013-05-14 13:46:13 -0700

    New NEWT_Id Helper

---
+++ application/controllers/helper_newt_id.php
@@ -0,0 +1,17 @@
+<?php
+require("base_controller.php");
+
+class helper_newt_id extends Base_controller {
+	// --------------------------------------------------------------------
+	function __construct()
+	{
+		// Call the parent constructor
+		parent::__construct();
+
+		$this->my_tag = "helper_newt_id";
+		$this->my_title = "NEWT ID Helper List Report";
+	}
+}
+
+
+?>
