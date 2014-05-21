ae2f5e93702a40e13b3318543e697ef0e635a2ea
alchemistmatt <matthew.monroe@pnnl.gov>
2014-05-20 18:21:36 -0700

    Add support for RNA_Prep_Requests

---
+++ application/controllers/rna_prep_request.php
@@ -0,0 +1,17 @@
+<?php
+require("base_controller.php");
+
+class rna_prep_request extends Base_controller {
+	// --------------------------------------------------------------------
+	function __construct()
+	{
+		// Call the parent constructor
+		parent::__construct();
+
+		$this->my_tag = "rna_prep_request";
+		$this->my_title = "RNA Prep Request";
+	}
+}
+
+
+?>
