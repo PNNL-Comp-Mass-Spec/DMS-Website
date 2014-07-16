a089255d9797ff878bf58c43711f3ae1e8959708
grkiebel@pnnl.gov <grkiebel@pnnl.gov>
2014-07-16 08:25:25 -0700

    OMCDA-1251

--- application/views/submission_cmd/sample_prep_cmds.php
+++
@@ -1,5 +0,0 @@
-<div id='message_contents' style='display:none'>
-<p>You have selected the 'Closed' state and there is biomaterial associated with this prep request.</p>
-<p>Would you like to change the state setting to special 'Closed (containers and material)' setting instead?</p>
-<p>This will retire the material and containers that are associated only with this prep request, and the request will be left in the 'Closed' state</p>
-</div>
