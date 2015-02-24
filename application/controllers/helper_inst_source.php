814bb283e33562dda46b8250ec89fb4dcdc2b805
alchemistmatt <matthew.monroe@pnnl.gov>
2015-02-24 14:25:14 -0800

    The instrument source file helper now shows a list of common DMS instruments if the $inst parameter is empty

--- application/controllers/helper_inst_source.php
+++ application/controllers/helper_inst_source.php
@@ -19,14 +19,58 @@ class helper_inst_source extends Base_controller {
 
 	// --------------------------------------------------------------------
 	// present file contents with chooser links
-	function view($inst)
+	function view($inst = "")
 	{
 		$this->load->helper(array('url', 'string'));
 
+		if (empty($inst)) {
+		
+			echo "<p>Commonly used DMS Instruments</p>";
+			echo "<ul>";
+			$instruments = array();
+			$instruments[] = "12T_FTICR_B";
+			$instruments[] = "15T_FTICR";
+			$instruments[] = "15T_FTICR_Imaging";
+			$instruments[] = "7T_FTICR_B";
+			$instruments[] = "Agilent_GC_MS_01";
+			$instruments[] = "CBSS_Orb1";
+			$instruments[] = "Exact03";
+			$instruments[] = "Exact04";
+			$instruments[] = "IMS04_AgTOF05";
+			$instruments[] = "IMS07_AgTOF04";
+			$instruments[] = "IMS08_AgQTOF05";
+			$instruments[] = "LTQ_2";
+			$instruments[] = "LTQ_4";
+			$instruments[] = "LTQ_Orb_1";
+			$instruments[] = "LTQ_Orb_2";
+			$instruments[] = "LTQ_Orb_3";
+			$instruments[] = "Maxis_01";
+			$instruments[] = "QExactHF03";
+			$instruments[] = "QExactP02";
+			$instruments[] = "TIMS_Maxis";
+			$instruments[] = "TSQ_3";
+			$instruments[] = "TSQ_4";
+			$instruments[] = "TSQ_5";
+			$instruments[] = "TSQ_6";
+			$instruments[] = "VOrbi05";
+			$instruments[] = "VOrbiETD01";
+			$instruments[] = "VOrbiETD02";
+			$instruments[] = "VOrbiETD03";
+			$instruments[] = "VOrbiETD04";
+			
+			foreach ($instruments as $instrument)
+			{
+				echo "<li><a href=\"/helper_inst_source/view/" . $instrument . "\">" . $instrument. "</a></li>";
+			}
+				
+		    exit;
+		}
+		
 		// get source content file from gigasax website
 		$file = fopen ("http://gigasax.pnl.gov/DMS_Inst_Source/".$inst."_source.txt", "r");
 		if (!$file) {
-		    echo "<p>Unable to open source file.\n";
+		    echo "<p>Unable to open source file.</p>";
+			echo "<p>See the list of <a href=\"/helper_inst_source/view/\">commonly used DMS instruments</a></li>";
 		    exit;
 		}
 
@@ -56,8 +100,13 @@ class helper_inst_source extends Base_controller {
 			// clean off file extensions
 			$valueClean = preg_replace('/(\.raw$|\.wiff$|\.d$)/i'  , '', $value );
 			
-			// make name into link, as appropriate (file or dir and no links for x_ marked)
-			if (($type == 'File' || $type == 'Dir') &&  !preg_match("/^x_/i", $valueClean)) {
+			// make name into link, as appropriate (file or dir)
+			// Do not link items that start with x_ or that end with .sld
+			if (($type == 'File' || $type == 'Dir') && 
+			    !preg_match("/^x_/i", $valueClean) &&
+			    !preg_match("/\.(sld|meth|txt|log)$/i", $value)
+			   ) 
+			{
 				$lnk = "<a href='javascript:opener.epsilon.updateFieldValueFromChooser(\"$valueClean\", \"replace\")' >$value</a>";
 			} else {
 				$lnk = $value;
