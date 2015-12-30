0ba22382f669579e60df9b5b7b3c6defc1e57d64
alchemistmatt <matthew.monroe@pnnl.gov>
2015-12-30 14:20:29 -0800

    Add Project_Usage report

---
+++ application/controllers/project_usage.php
@@ -0,0 +1,17 @@
+<?php
+require("base_controller.php");
+
+class project_usage extends Base_controller {
+	// --------------------------------------------------------------------
+	function __construct()
+	{
+		// Call the parent constructor
+		parent::__construct();
+
+		$this->my_tag = "project_usage";
+		$this->my_title = "Project Usage Stats";
+	}
+}
+
+
+?>
