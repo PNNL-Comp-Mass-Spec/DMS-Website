2774764d7e060429f46b13b013ae8c15c214e619
grkiebel@gmail.gov <grkiebel@gmail.gov>
2016-02-10 07:59:45 -0800

    Bug fix

--- application/controllers/File_attachment.php
+++ application/controllers/File_attachment.php
@@ -306,7 +306,6 @@ class File_attachment extends Base_controller {
 			if(!$remote->ok) throw new Exception($remote->message);
 
 	 		$result = $this->get_valid_file_path($entity_type, $entity_id, $filename);
-//echo "-->" . $result ."<--";
 	 		
 		    if(!$result->ok) throw new Exception($result->message);
  			$full_path = $result->path;
