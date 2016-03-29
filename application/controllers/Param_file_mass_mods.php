a30832d908d788a5d8b3f6b18267166d83b7adc9
alchemistmatt <matthew.monroe@pnnl.gov>
2016-03-28 19:13:06 -0700

    Add Mass_correction_factors and Param_file_mass_mods page familes

---
+++ application/controllers/Param_file_mass_mods.php
@@ -0,0 +1,17 @@
+<?php
+require("Base_controller.php");
+
+class param_file_mass_mods extends Base_controller {
+	// --------------------------------------------------------------------
+	function __construct()
+	{
+		// Call the parent constructor
+		parent::__construct();
+
+		$this->my_tag = "param_file_mass_mods";
+		$this->my_title = "Param File Mass Mods";
+	}
+}
+
+
+?>
