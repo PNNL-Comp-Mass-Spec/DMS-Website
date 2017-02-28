3b4d18227a78c873df8acc5286f6df58d7071945
alchemistmatt <matthew.monroe@pnnl.gov>
2017-02-27 16:19:51 -0800

    Change the title of the lc cart config history page family from "LC Cart Config" to "LC Cart Service Note"

--- application/controllers/Lc_cart_config_history.php
+++ application/controllers/Lc_cart_config_history.php
@@ -9,7 +9,7 @@ class Lc_cart_config_history extends Base_controller {
 		parent::__construct();
 
 		$this->my_tag = "lc_cart_config_history";
-		$this->my_title = "LC Cart Config";
+		$this->my_title = "LC Cart Service Note";
 	}
 }
 
