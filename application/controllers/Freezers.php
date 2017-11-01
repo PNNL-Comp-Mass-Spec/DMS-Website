2b3c71d65b1caca2c1d337ccbfbd3b4243a3574c
alchemistmatt <matthew.monroe@pnnl.gov>
2017-11-01 15:25:39 -0700

    Add Freezers page family

---
+++ application/controllers/Freezers.php
@@ -0,0 +1,17 @@
+<?php
+require("Base_controller.php");
+
+class freezers extends Base_controller {
+	// --------------------------------------------------------------------
+	function __construct()
+	{
+		// Call the parent constructor
+		parent::__construct();
+
+		$this->my_tag = "freezers";
+		$this->my_title = "Freezer";
+	}
+}
+
+
+?>
