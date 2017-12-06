0fff0d75cfa0ef502f621143e9b1a97581f63dca
alchemistmatt <matthew.monroe@pnnl.gov>
2017-12-06 15:37:45 -0800

    Add reference compound page families

---
+++ application/controllers/Helper_reference_compound.php
@@ -0,0 +1,17 @@
+<?php
+require("Base_controller.php");
+
+class helper_reference_compound extends Base_controller {
+	// --------------------------------------------------------------------
+	function __construct()
+	{
+		// Call the parent constructor
+		parent::__construct();
+
+		$this->my_tag = "helper_reference_compound";
+		$this->my_title = "Reference Compound Helper";
+	}
+}
+
+
+?>
