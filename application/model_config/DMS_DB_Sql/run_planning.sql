b51a65fbc5486af92aca3a86c6490d7c74d45afb
alchemistmatt <matthew.monroe@pnnl.gov>
2019-03-29 13:46:46 -0700

    On the run planning report, rename a primary filter and link on Batch ID

--- application/model_config/DMS_DB_Sql/run_planning.sql
+++ application/model_config/DMS_DB_Sql/run_planning.sql
@@ -7,7 +7,7 @@ INSERT INTO "general_params" VALUES('list_report_data_order_by','[Inst. Group], 
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO "list_report_primary_filter" VALUES(1,'pf_instrument','Inst. Group','32','','Inst. Group','ContainsText','text','128','','');
 INSERT INTO "list_report_primary_filter" VALUES(2,'pf_min_request','Min Request','32','','Min Request','ContainsText','text','128','','');
-INSERT INTO "list_report_primary_filter" VALUES(3,'pf_request_name','Request Name','32','','Request Name','ContainsText','text','128','','');
+INSERT INTO "list_report_primary_filter" VALUES(3,'pf_request_or_batch_name','Request/Batch Name','32','','Request or Batch Name','ContainsText','text','128','','');
 INSERT INTO "list_report_primary_filter" VALUES(4,'pf_requester','Requester','32','','Requester','ContainsText','text','128','','');
 INSERT INTO "list_report_primary_filter" VALUES(5,'pf_batch_id','Batch','16','','Batch','Equals','text','16','','');
 INSERT INTO "list_report_primary_filter" VALUES(6,'pf_comment','Comment','32','','Comment','ContainsText','text','128','','');
@@ -20,4 +20,5 @@ INSERT INTO "list_report_hotlinks" VALUES(3,'Run Count','invoke_entity','Request
 INSERT INTO "list_report_hotlinks" VALUES(4,'Work Package','invoke_entity','value','charge_code/show/','');
 INSERT INTO "list_report_hotlinks" VALUES(5,'WP State','color_label','#WPActivationState','','{"0":"clr_30","1":"clr_45","2":"clr_60","3":"clr_120","4":"clr_120","5":"clr_120"}');
 INSERT INTO "list_report_hotlinks" VALUES(6,'Proposal','invoke_entity','value','eus_proposals/show','');
+INSERT INTO "list_report_hotlinks" VALUES(7,'Batch','invoke_entity','value','requested_run_batch/show','');
 COMMIT;
