83c5742cd8841a7302cbd42f5fef237c8d911834
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-03 18:34:49 -0700

    Add comment

--- javascript/tracking.js
+++ javascript/tracking.js
@@ -45,6 +45,7 @@ var tracking = {
 			this.updateDatabaseFromList(flist, id_type);
 		},
 		reloadReport: function(operation) {
+			// Call stored procedure UpdateInstrumentUsageReport with @operation set to either 'refresh' or 'reload'
 			var url =  gamma.pageContext.ops_url;
 			var p = {};
 			p.factorList = '';
