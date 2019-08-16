ca5a51a0657d5fb9a2a8396747db916384e19c38
alchemistmatt <matthew.monroe@pnnl.gov>
2019-08-16 15:56:04 -0700

    New InstrumentID page family

--- application/controllers/Instrumentid.php
+++ application/controllers/Instrumentid.php
@@ -1,7 +1,7 @@
 <?php
 require("Base_controller.php");
 
-class Instrument extends Base_controller {
+class Instrumentid extends Base_controller {
 
 
     // --------------------------------------------------------------------
@@ -10,8 +10,8 @@ class Instrument extends Base_controller {
         // Call the parent constructor
         parent::__construct();
 
-        $this->my_tag = "instrument";
-        $this->my_title = "Instrument";
+        $this->my_tag = "instrumentid";
+        $this->my_title = "Instrument ID";
     }
 
 }
