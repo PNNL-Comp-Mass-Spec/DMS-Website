12b7ec3929f5a2c594ebb8020400292fedd7e5b6
alchemistmatt <matthew.monroe@pnnl.gov>
2019-10-07 18:37:16 -0700

    Replace double quotes with &quot; to avoid javascript exceptions

--- javascript/dms2.js
+++ javascript/dms2.js
@@ -422,9 +422,10 @@ var gamma = {
 						data = '<pre>' + formattedSql + '</pre>';
 
 						// Text in dataForClipboard is plain text and will appear when pasting into a text editor or SQL Server Management Studio
-						// Text in htmlForClipboard includes HTML symbols, and will appear when pasted into Microsoft Word
-						dataForClipboard = unformattedSql
-						htmlForClipboard = data;
+						// Text in htmlForClipboard includes HTML symbols, and will appear when pasted into Microsoft Word                        
+                        // Replace double quotes with &quot; to avoid javascript exceptions                        
+						dataForClipboard = unformattedSql.replace(/"/g, '&quot;');
+						htmlForClipboard = data.replace(/"/g, '&quot;');
 
 					} else {
 						var buttonName = 'copy-url-now';
