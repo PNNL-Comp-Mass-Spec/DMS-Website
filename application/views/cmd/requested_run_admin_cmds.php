d2eab881133cfe4c71f17b06b09f52fc4e61c8fb
alchemistmatt <matthew.monroe@pnnl.gov>
2020-10-21 17:41:11 -0700

    Rearrange buttons

--- application/views/cmd/requested_run_admin_cmds.php
+++ application/views/cmd/requested_run_admin_cmds.php
@@ -2,6 +2,28 @@
 
 <form name="DBG" action="">
 
+<hr>
+<div>
+<!-- "assignedInstrument" will be passed to the @mode parameter of stored procedure UpdateRequestedRunAssignments -->
+<!-- The value selected in the chooser will be sent to the @newValue parameter -->
+<input class="button lst_cmd_btn" type="button" value="Update" 
+       onClick='lcmd.requested_run_admin.op("assignedInstrument", "instrument_name_chooser")' />
+Assign the selected requests to instrument
+<span><?= $this->choosers->get_chooser('instrument_name', 'instrumentNamePickList')?></span>
+</div>
+
+<div>
+<!-- 
+  tau.requested_run_admin.setRequestStatus is defined in factors.js
+  It calls stored procedure UpdateRequestedRunAdmin, as defined in https://dms2.pnl.gov/config_db/show_db/requested_run_admin.db
+  It does this by examining the POST data then calling the stored procedure defined by operations_sproc
+-->
+<input class='button lst_cmd_btn' type="button" value="Unassign Requests"
+       onClick='tau.requested_run_admin.setRequestStatus("UnassignInstrument")'
+       title="Unassign"  /> Unassign selected requests from the queued instrument
+</div>
+
+<hr>
 <!-- 
   lcmd.requested_run_admin.op will POST to requested_run/operation/ , 
   which in turn will call UpdateRequestedRunAssignments, 
@@ -33,27 +55,6 @@ Change dataset type of selected requests to
        onClick='lcmd.requested_run_admin.op("separationGroup", "separation_group_chooser")' />
 Change separation group of selected requests to
 <span><?= $this->choosers->get_chooser('separation_group', 'separationGroupPickList')?></span>
-</div>
-
-<hr>
-<div>
-<!-- "assignedInstrument" will be passed to the @mode parameter of stored procedure UpdateRequestedRunAssignments -->
-<!-- The value selected in the chooser will be sent to the @newValue parameter -->
-<input class="button lst_cmd_btn" type="button" value="Update" 
-       onClick='lcmd.requested_run_admin.op("assignedInstrument", "instrument_name_chooser")' />
-Assign the selected requests to instrument
-<span><?= $this->choosers->get_chooser('instrument_name', 'instrumentNamePickList')?></span>
-</div>
-
-<div>
-<!-- 
-  tau.requested_run_admin.setRequestStatus is defined in factors.js
-  It calls stored procedure UpdateRequestedRunAdmin, as defined in https://dms2.pnl.gov/config_db/show_db/requested_run_admin.db
-  It does this by examining the POST data then calling the stored procedure defined by operations_sproc
--->
-<input class='button lst_cmd_btn' type="button" value="Unassign Requests"
-       onClick='tau.requested_run_admin.setRequestStatus("UnassignInstrument")'
-       title="Unassign"  /> Unassign selected requests from the queued instrument
 </div>
 
 <hr>
