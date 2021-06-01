5e4b04efd087f41be53ebf7477c75340c53dc314
alchemistmatt <matthew.monroe@pnnl.gov>
2021-05-31 17:59:54 -0700

    Fix button name bug for analysis job requests

--- application/model_config/DMS_DB_Sql/analysis_job_request.sql
+++ application/model_config/DMS_DB_Sql/analysis_job_request.sql
@@ -77,7 +77,7 @@ INSERT INTO "detail_report_hotlinks" VALUES(8,'Parameter File','detail-report','
 INSERT INTO "detail_report_hotlinks" VALUES(9,'Legacy Fasta','detail-report','Legacy Fasta','helper_organism_db/report/~','valueCol','dl_legacy_fasta','');
 INSERT INTO "detail_report_hotlinks" VALUES(10,'Data Package ID','detail-report','Data Package ID','data_package/show','valueCol','dl_data_package','');
 CREATE TABLE entry_commands ( id INTEGER PRIMARY KEY,  "name" text, "type" text, "label" text, "tooltip" text, "target" text );
-INSERT INTO "entry_commands" VALUES(1,'preview','cmd','Preview','Determine if current values are valid, but do not change database.','');
+INSERT INTO "entry_commands" VALUES(1,'PreviewAdd','cmd','Preview Add','Determine if current values are valid, but do not change database.','');
 CREATE TABLE external_sources ( id INTEGER PRIMARY KEY,  "source_page" text, "field" text, "type" text, "value" text );
 INSERT INTO "external_sources" VALUES(1,'analysis_job','AJR_requestID','Literal','0');
 INSERT INTO "external_sources" VALUES(2,'analysis_job','AJR_requestName','Literal','New Request');
