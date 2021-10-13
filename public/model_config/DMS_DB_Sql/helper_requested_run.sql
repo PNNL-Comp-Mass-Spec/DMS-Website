b051686bb07e704f1ad8d7f4f64062b427d0aa05
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-12 18:16:27 -0700

    Sort by descending Request ID on the requested run helper

--- public/model_config/DMS_DB_Sql/helper_requested_run.sql
+++ public/model_config/DMS_DB_Sql/helper_requested_run.sql
@@ -2,7 +2,7 @@
 BEGIN TRANSACTION;
 CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO "general_params" VALUES('list_report_data_table','V_Requested_Run_Helper_List_Report');
-INSERT INTO "general_params" VALUES('list_report_data_sort_col','Created');
+INSERT INTO "general_params" VALUES('list_report_data_sort_col','Request');
 INSERT INTO "general_params" VALUES('list_report_data_sort_dir','DESC');
 INSERT INTO "general_params" VALUES('list_report_helper_multiple_selection','yes');
 INSERT INTO "general_params" VALUES('list_report_data_cols','Request AS Sel,  Request, Name, Experiment, Instrument, Requestor, Wellplate, Created');
