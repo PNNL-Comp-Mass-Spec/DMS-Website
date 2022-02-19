2ee4ddbc25f9d959d82e7dbd4f8e6a64f2a833a8
alchemistmatt <matthew.monroe@pnnl.gov>
2022-02-18 16:46:39 -0800

    Add dataset_tracking alias

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -58,6 +58,8 @@ $routes->getAlias('data_package_jobs', 'Data_package_analysis_jobs');
 
 $routes->getAlias('dataset_files', 'Dataset_file');
 
+$routes->getAlias('dataset_tracking', 'Tracking_dataset');
+
 $routes->getAlias('datasets', 'Dataset');
 
 $routes->getAlias('eus_user', 'Eus_users');
