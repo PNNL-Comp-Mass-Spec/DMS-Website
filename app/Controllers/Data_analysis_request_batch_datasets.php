16199b1b85e450a1ce83f94e46f070f1ce3c3f8d
alchemistmatt <matthew.monroe@pnnl.gov>
2022-03-26 17:30:12 -0700

    Add data analysis request batch datasets page family

---
+++ app/Controllers/Data_analysis_request_batch_datasets.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Data_analysis_request_batch_datasets extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "data_analysis_request_batch_datasets";
+        $this->my_title = "Data Analysis Request Batch Datasets";
+    }
+}
+?>
