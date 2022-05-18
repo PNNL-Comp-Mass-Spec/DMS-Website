6a51e43bfbcce22463e299529ea5c554bb9e01e9
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-18 16:57:29 -0700

    Update form field names

--- public/model_config/DMS_DB_Sql/settings_files.sql
+++ public/model_config/DMS_DB_Sql/settings_files.sql
@@ -7,18 +7,18 @@ INSERT INTO "general_params" VALUES('detail_report_data_table','V_Settings_Files
 INSERT INTO "general_params" VALUES('detail_report_data_id_col','ID');
 INSERT INTO "general_params" VALUES('detail_report_data_id_type','integer');
 INSERT INTO "general_params" VALUES('entry_sproc','AddUpdateSettingsFile');
-INSERT INTO "general_params" VALUES('entry_page_data_table','V_Settings_Files_Entry');
-INSERT INTO "general_params" VALUES('entry_page_data_id_col','ID');
-INSERT INTO "general_params" VALUES('post_submission_detail_id','ID');
+INSERT INTO "general_params" VALUES('entry_page_data_table','v_settings_files_entry');
+INSERT INTO "general_params" VALUES('entry_page_data_id_col','id');
+INSERT INTO "general_params" VALUES('post_submission_detail_id','id');
 CREATE TABLE form_fields ( id INTEGER PRIMARY KEY, "name"  text, "label" text, "type" text, "size" text, "maxlength" text, "rows" text, "cols" text, "default" text, "rules" text);
-INSERT INTO "form_fields" VALUES(1,'ID','ID','non-edit','','','','','','trim');
-INSERT INTO "form_fields" VALUES(2,'AnalysisTool','Analysis Tool','text','60','64','','','','trim|max_length[64]');
-INSERT INTO "form_fields" VALUES(3,'FileName','FileName','area','','','1','80','','trim|max_length[255]');
-INSERT INTO "form_fields" VALUES(4,'Description','Description','area','','','4','80','','trim|max_length[1024]');
-INSERT INTO "form_fields" VALUES(5,'Active','Active','text','1','1','','','1','trim|max_length[1]');
-INSERT INTO "form_fields" VALUES(6,'AutoCentroid','AutoCentroid','area','','','1','80','','trim|max_length[255]');
-INSERT INTO "form_fields" VALUES(7,'HMS_AutoSupersede','HMS_AutoSupersede','area','','','1','80','','trim|max_length[255]');
-INSERT INTO "form_fields" VALUES(8,'Contents','Contents','area','','','18','80','<section name="AAA" tool="">
+INSERT INTO "form_fields" VALUES(1,'id','ID','non-edit','','','','','','trim');
+INSERT INTO "form_fields" VALUES(2,'analysis_tool','Analysis Tool','text','60','64','','','','trim|max_length[64]');
+INSERT INTO "form_fields" VALUES(3,'file_name','FileName','area','','','1','80','','trim|max_length[255]');
+INSERT INTO "form_fields" VALUES(4,'description','Description','area','','','4','80','','trim|max_length[1024]');
+INSERT INTO "form_fields" VALUES(5,'active','Active','text','1','1','','','1','trim|max_length[1]');
+INSERT INTO "form_fields" VALUES(6,'auto_centroid','AutoCentroid','area','','','1','80','','trim|max_length[255]');
+INSERT INTO "form_fields" VALUES(7,'hms_auto_supersede','HMS_AutoSupersede','area','','','1','80','','trim|max_length[255]');
+INSERT INTO "form_fields" VALUES(8,'contents','Contents','area','','','18','80','<section name="AAA" tool="">
 
 	<item key="xxx" value=""></item>
 
@@ -34,9 +34,9 @@ INSERT INTO "form_fields" VALUES(8,'Contents','Contents','area','','','18','80',
 
 </section>','trim|max_length[2147483647]');
 CREATE TABLE form_field_options ( id INTEGER PRIMARY KEY,  "field" text, "type" text, "parameter" text );
-INSERT INTO "form_field_options" VALUES(1,'Contents','auto_format','xml');
+INSERT INTO "form_field_options" VALUES(1,'contents','auto_format','xml');
 CREATE TABLE form_field_choosers ( id INTEGER PRIMARY KEY,  "field" text, "type" text, "PickListName" text, "Target" text, "XRef" text, "Delimiter" text, "Label" text);
-INSERT INTO "form_field_choosers" VALUES(1,'AnalysisTool','picker.replace','analysisToolPickList','','',',','');
+INSERT INTO "form_field_choosers" VALUES(1,'analysis_tool','picker.replace','analysisToolPickList','','',',','');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO "list_report_primary_filter" VALUES(1,'pf_analysis_tool','Analysis Tool','20','','Analysis Tool','ContainsText','text','128','','');
 INSERT INTO "list_report_primary_filter" VALUES(2,'pf_file_name','File Name','35!','','File Name','ContainsText','text','128','','');
@@ -46,14 +46,14 @@ INSERT INTO "list_report_hotlinks" VALUES(1,'ID','invoke_entity','value','settin
 INSERT INTO "list_report_hotlinks" VALUES(2,'Description','min_col_width','value','200','');
 INSERT INTO "list_report_hotlinks" VALUES(3,'Analysis Tool','invoke_entity','value','analysis_job/report/-/-/~@/-/-/-/-/-/52/-','');
 CREATE TABLE sproc_args ( id INTEGER PRIMARY KEY, "field" text, "name" text, "type" text, "dir" text, "size" text, "procedure" text);
-INSERT INTO "sproc_args" VALUES(1,'ID','settingsFileID','int','output','','AddUpdateSettingsFile');
-INSERT INTO "sproc_args" VALUES(2,'AnalysisTool','analysisTool','varchar','input','64','AddUpdateSettingsFile');
-INSERT INTO "sproc_args" VALUES(3,'FileName','fileName','varchar','input','255','AddUpdateSettingsFile');
-INSERT INTO "sproc_args" VALUES(4,'Description','description','varchar','input','1024','AddUpdateSettingsFile');
-INSERT INTO "sproc_args" VALUES(5,'Active','active','tinyint','input','','AddUpdateSettingsFile');
-INSERT INTO "sproc_args" VALUES(6,'Contents','contents','text','input','2147483647','AddUpdateSettingsFile');
-INSERT INTO "sproc_args" VALUES(7,'HMS_AutoSupersede','hmsAutoSupersede','varchar','input','255','AddUpdateSettingsFile');
-INSERT INTO "sproc_args" VALUES(8,'AutoCentroid','msgfPlusAutoCentroid','varchar','input','255','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(1,'id','settingsFileID','int','output','','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(2,'analysis_tool','analysisTool','varchar','input','64','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(3,'file_name','fileName','varchar','input','255','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(4,'description','description','varchar','input','1024','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(5,'active','active','tinyint','input','','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(6,'contents','contents','text','input','2147483647','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(7,'hms_auto_supersede','hmsAutoSupersede','varchar','input','255','AddUpdateSettingsFile');
+INSERT INTO "sproc_args" VALUES(8,'auto_centroid','msgfPlusAutoCentroid','varchar','input','255','AddUpdateSettingsFile');
 INSERT INTO "sproc_args" VALUES(9,'<local>','mode','varchar','input','12','AddUpdateSettingsFile');
 INSERT INTO "sproc_args" VALUES(10,'<local>','message','varchar','output','512','AddUpdateSettingsFile');
 INSERT INTO "sproc_args" VALUES(11,'<local>','callingUser','varchar','input','128','AddUpdateSettingsFile');
