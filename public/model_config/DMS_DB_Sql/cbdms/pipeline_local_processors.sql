a1b90a26cf9d6e97c904c080e0f451da52b21b7a
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-24 16:52:14 -0700

    Remove unused hotlink definitions

--- public/model_config/DMS_DB_Sql/cbdms/pipeline_local_processors.sql
+++ public/model_config/DMS_DB_Sql/cbdms/pipeline_local_processors.sql
@@ -9,7 +9,4 @@ INSERT INTO "general_params" VALUES('my_db_group','broker');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO "list_report_primary_filter" VALUES(1,'pf_processor_name','Processor Name','6','','Processor Name','ContainsText','text','80','','');
 INSERT INTO "list_report_primary_filter" VALUES(2,'pf_machine','Machine','6','','Machine','ContainsText','text','80','','');
-CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
-INSERT INTO "list_report_hotlinks" VALUES(1,'Job','invoke_entity','value','pipeline_local_processors/show/','');
-INSERT INTO "list_report_hotlinks" VALUES(2,'Steps','invoke_entity','Job','pipeline_job_steps/report','');
 COMMIT;
