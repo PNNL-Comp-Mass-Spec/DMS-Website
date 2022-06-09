e21e762e2ce59582dfc48c32ce42209af0fbff4d
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-09 11:21:27 -0700

    Add/update column to sort on

--- public/model_config/DMS_DB_Sql/cbdms/event_log_analysis_job.sql
+++ public/model_config/DMS_DB_Sql/cbdms/event_log_analysis_job.sql
@@ -3,6 +3,7 @@ BEGIN TRANSACTION;
 CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO "general_params" VALUES('list_report_data_table','V_Event_Log_Analysis_Job_List');
 INSERT INTO "general_params" VALUES('list_report_data_sort_dir','DESC');
+INSERT INTO "general_params" VALUES('list_report_data_sort_col','Event_ID');
 CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
 INSERT INTO "list_report_hotlinks" VALUES(1,'Job','invoke_entity','value','analysis_job/show','');
 COMMIT;
