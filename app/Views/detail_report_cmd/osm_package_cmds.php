794d8d9fabfd5264b12da1a9963a2972f24d9a74
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-21 11:16:07 -0700

    Always hide delete button

--- app/Views/detail_report_cmd/osm_package_cmds.php
+++ app/Views/detail_report_cmd/osm_package_cmds.php
@@ -1,7 +1,8 @@
 
-<!-- check permission and limit visibility -->
+<!-- check permission and limit visibility (deprecated option) -->
 
-<?php if ($check_access('operation', false)): ?>
+<!-- < php if ($check_access('operation', false)): > -->
+     <?php if (false):                            ?>
 <div id='osm_cmd_container' style='margin-top:4px;'>
 <input class='button lst_cmd_btn' type='button' value='Delete Package' onclick='packages.updateOSMPackage("<?= $id ?>", "delete")' title="Delete this OSM Package" />
 </div>
