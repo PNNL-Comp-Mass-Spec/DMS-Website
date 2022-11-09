0bbeea41b4bddb7d3c2697beb72d23578e22584f
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-09 13:50:14 -0800

    Use new page family name

--- app/Controllers/Predefined_analysis_jobs_preview.php
+++ app/Controllers/Predefined_analysis_jobs_preview.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Controllers;
 
-class Predefined_analysis_preview extends DmsBase {
+class Predefined_analysis_jobs_preview extends DmsBase {
     function __construct()
     {
-        $this->my_tag = "predefined_analysis_preview";
-        $this->my_title = "Preview Predefined Analyses";
+        $this->my_tag = "predefined_analysis_jobs_preview";
+        $this->my_title = "Preview Predefined Analysis Jobs";
     }
 }
 ?>
