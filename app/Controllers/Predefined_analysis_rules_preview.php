ba36573c91d45a22e83635f977b4e1129fe94c8b
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-09 13:51:06 -0800

    Add new page family to preview predefined analysis job rules

---
+++ app/Controllers/Predefined_analysis_rules_preview.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Predefined_analysis_rules_preview extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "predefined_analysis_rules_preview";
+        $this->my_title = "Preview Predefined Analysis Job Rules";
+    }
+}
+?>
