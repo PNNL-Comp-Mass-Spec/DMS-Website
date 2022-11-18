4c5e6899f02f2af242a7828927b2bc200f701d6e
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-18 13:30:12 -0800

    Use new procedure names for previewing predefine rules

--- public/model_config/DMS_DB_Sql/cbdms/predefined_analysis_preview_mds.sql
+++ public/model_config/DMS_DB_Sql/cbdms/predefined_analysis_preview_mds.sql
@@ -2,7 +2,7 @@
 BEGIN TRANSACTION;
 CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO general_params VALUES('list_report_data_sort_dir','DESC');
-INSERT INTO general_params VALUES('list_report_sproc','EvaluatePredefinedAnalysisRulesMDS');
+INSERT INTO general_params VALUES('list_report_sproc','predefined_analysis_jobs_mds_proc');
 CREATE TABLE form_fields ( id INTEGER PRIMARY KEY, "name"  text, "label" text, "type" text, "size" text, "maxlength" text, "rows" text, "cols" text, "default" text, "rules" text);
 INSERT INTO form_fields VALUES(1,'datasetList','Datasets','area','','','4','60','QC_05_3_f_22Jan07_Phoenix_06-11-19, core_BO215_050601B_06Jun05_Agilent_0305-12s, ShewFed089_LTQFT2_23Feb06_Griffin_06-01-09','trim|required|max_length[3500]');
 CREATE TABLE form_field_choosers ( id INTEGER PRIMARY KEY,  "field" text, "type" text, "PickListName" text, "Target" text, "XRef" text, "Delimiter" text, "Label" text);
@@ -12,6 +12,6 @@ INSERT INTO list_report_hotlinks VALUES(1,'Dataset','invoke_entity','value','dat
 INSERT INTO list_report_hotlinks VALUES(2,'Jobs','invoke_entity','Dataset','analysis_job/report/-/-/-/~','');
 INSERT INTO list_report_hotlinks VALUES(3,'Job','row_to_url','Dataset','analysis_group/create/predefined_analysis_preview/post','');
 CREATE TABLE sproc_args ( id INTEGER PRIMARY KEY, "field" text, "name" text, "type" text, "dir" text, "size" text, "procedure" text);
-INSERT INTO sproc_args VALUES(1,'datasetList','datasetList','varchar','input','3500','EvaluatePredefinedAnalysisRulesMDS');
-INSERT INTO sproc_args VALUES(2,'<local>','message','varchar','output','512','EvaluatePredefinedAnalysisRulesMDS');
+INSERT INTO sproc_args VALUES(1,'datasetList','datasetList','varchar','input','3500','predefined_analysis_jobs_mds_proc');
+INSERT INTO sproc_args VALUES(2,'<local>','message','varchar','output','512','predefined_analysis_jobs_mds_proc');
 COMMIT;
