be8d1f600c089b2439c6e991226e45c710bfca97
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-19 21:19:54 -0800

    Add new experiment group helper

---
+++ app/Controllers/Helper_experiment_group_name.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Helper_experiment_group_name extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "helper_experiment_group_name";
+        $this->my_title = "Experiment Group Helper List Report";
+    }
+}
+?>
--- public/model_config/DMS_DB_Sql/helper_experiment_group_name.sql
+++ public/model_config/DMS_DB_Sql/helper_experiment_group_name.sql
@@ -5,12 +5,12 @@ INSERT INTO general_params VALUES('list_report_data_table','V_Experiment_Groups_
 INSERT INTO general_params VALUES('list_report_data_sort_col','ID');
 INSERT INTO general_params VALUES('list_report_data_sort_dir','DESC');
 INSERT INTO general_params VALUES('list_report_helper_multiple_selection','no');
+CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
+INSERT INTO list_report_hotlinks VALUES(1,'Group_Name','update_opener','value','','');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO list_report_primary_filter VALUES(1,'pf_id','ID','32','','ID','Equals','text','32','','');
 INSERT INTO list_report_primary_filter VALUES(2,'pf_name','Group Name','32','','Group_Name','ContainsText','text','128','','');
 INSERT INTO list_report_primary_filter VALUES(3,'pf_description','Description','32','','Description','ContainsText','text','128','','');
 INSERT INTO list_report_primary_filter VALUES(4,'pf_parent_experiment','Parent Experiment','32','','Parent_Experiment','ContainsText','text','128','','');
 INSERT INTO list_report_primary_filter VALUES(5,'pf_group_type','Group ype','32','','Group_Type','ContainsText','text','128','','');
-CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
-INSERT INTO list_report_hotlinks VALUES(1,'ID','update_opener','value','','');
 COMMIT;
