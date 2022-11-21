0b9a17e3f78fff9c71ef6933c15d784a57fe5774
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-20 19:28:52 -0800

    Remove extra text from page family title

--- app/Controllers/Helper_experiment_group_name.php
+++ app/Controllers/Helper_experiment_group_name.php
@@ -5,7 +5,7 @@ class Helper_experiment_group_name extends DmsBase {
     function __construct()
     {
         $this->my_tag = "helper_experiment_group_name";
-        $this->my_title = "Experiment Group Helper List Report";
+        $this->my_title = "Experiment Group Helper";
     }
 }
 ?>
