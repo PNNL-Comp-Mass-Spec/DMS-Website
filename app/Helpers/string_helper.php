eaec5a12751775ec4e550b5a2c437f48749bc5ce
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2022-12-16 16:27:03 -0800

    Update string_helper.php to not cause exceptions if loaded multiple times

--- app/Helpers/string_helper.php
+++ app/Helpers/string_helper.php
@@ -1,46 +1,58 @@
 <?php
 
-/**
- * Return True if the variable is null or empty (aka IsNullOrEmpty)
- * @param type $variable
- * @return type
- */
-function IsNullOrWhiteSpace($variable) {
-    return (!isset($variable) || trim($variable) === '');
-}
-
-/**
- * Return True if the variable is defined and is not whitespace
- * @param type $variable
- * @return type
- */
-function IsNotWhitespace($variable) {
-    return (isset($variable) && trim($variable) !== '');
-}
-
-/**
- * Return true if $haystack starts with $needle
- * @param type $haystack
- * @param type $needle
- * @return type
- */
-function StartsWith($haystack, $needle) {
-    $length = strlen($needle);
-    return (substr($haystack, 0, $length) === $needle);
-}
-
-/**
- * Return true if $haystack ends with $needle
- * @param type $haystack
- * @param type $needle
- * @return type
- */
-function EndsWith($haystack, $needle) {
-    if (strlen($needle) > strlen($haystack)) {
-        return false;
+if (! function_exists('IsNullOrWhiteSpace'))
+{
+    /**
+     * Return True if the variable is null or empty (aka IsNullOrEmpty)
+     * @param type $variable
+     * @return type
+     */
+    function IsNullOrWhiteSpace($variable) {
+        return (!isset($variable) || trim($variable) === '');
     }
+}
 
-    $length = strlen($needle);
-    return (substr($haystack, strlen($haystack) - $length, $length) === $needle);
+if (! function_exists('IsNotWhitespace'))
+{
+    /**
+     * Return True if the variable is defined and is not whitespace
+     * @param type $variable
+     * @return type
+     */
+    function IsNotWhitespace($variable) {
+        return (isset($variable) && trim($variable) !== '');
+    }
+}
+
+if (! function_exists('StartsWith'))
+{
+    /**
+     * Return true if $haystack starts with $needle
+     * @param type $haystack
+     * @param type $needle
+     * @return type
+     */
+    function StartsWith($haystack, $needle) {
+        $length = strlen($needle);
+        return (substr($haystack, 0, $length) === $needle);
+    }
+}
+
+if (! function_exists('EndsWith'))
+{
+    /**
+     * Return true if $haystack ends with $needle
+     * @param type $haystack
+     * @param type $needle
+     * @return type
+     */
+    function EndsWith($haystack, $needle) {
+        if (strlen($needle) > strlen($haystack)) {
+            return false;
+        }
+
+        $length = strlen($needle);
+        return (substr($haystack, strlen($haystack) - $length, $length) === $needle);
+    }
 }
 ?>
