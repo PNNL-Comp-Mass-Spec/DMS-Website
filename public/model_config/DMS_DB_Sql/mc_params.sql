74bc587fee7d569491f3b97a1f40562b3d6a31c7
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-31 21:07:00 -0800

    Remove explicit list of columns

--- public/model_config/DMS_DB_Sql/mc_params.sql
+++ public/model_config/DMS_DB_Sql/mc_params.sql
@@ -4,7 +4,6 @@ CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO general_params VALUES('base_table','V_Param_Value');
 INSERT INTO general_params VALUES('my_db_group','manager_control');
 INSERT INTO general_params VALUES('list_report_data_table','v_param_value');
-INSERT INTO general_params VALUES('list_report_data_cols','mgr_name AS [Mgr_Name], param_name AS [Param_Name], entry_id AS [Entry_ID], type_id AS [Type_ID], value AS [Value], mgr_id AS [Mgr_ID], comment AS [Comment], last_affected AS [Last_Affected], entered_by AS [Entered_By], mgr_type_id AS [Mgr_Type_ID]');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO list_report_primary_filter VALUES(1,'pf_m_name','Mgr_Name','20','','mgr_name','ContainsText','text','50','','');
 INSERT INTO list_report_primary_filter VALUES(2,'pf_paramname','Param_Name','30!','','param_name','ContainsText','text','128','','');
