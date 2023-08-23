801bde704fa5caf7767b5f5fd13a8b6296058602
alchemistmatt <matthew.monroe@pnnl.gov>
2023-08-23 16:40:38 -0700

    Remove column name that does not exist in view v_active_requested_runs

--- public/model_config/DMS_DB_Sql/helper_scheduled_run.sql
+++ public/model_config/DMS_DB_Sql/helper_scheduled_run.sql
@@ -4,7 +4,7 @@ CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO general_params VALUES('list_report_data_table','v_active_requested_runs');
 INSERT INTO general_params VALUES('list_report_data_sort_col','created');
 INSERT INTO general_params VALUES('list_report_data_sort_dir','DESC');
-INSERT INTO general_params VALUES('list_report_data_cols','request, name, instrument, experiment, requester, created, comment, note, wellplate, well');
+INSERT INTO general_params VALUES('list_report_data_cols','request, name, instrument, experiment, requester, created, comment, wellplate, well');
 INSERT INTO general_params VALUES('list_report_helper_multiple_selection','no');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO list_report_primary_filter VALUES(1,'pf_name','Name','32','','name','ContainsText','text','128','','');
