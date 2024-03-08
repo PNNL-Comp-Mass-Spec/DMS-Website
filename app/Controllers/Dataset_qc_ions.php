f8b4ec2eb69d609be8029f8142a7d0e41b966a43
alchemistmatt <matthew.monroe@pnnl.gov>
2024-03-08 10:31:55 -0800

    Add Dataset QC Ions page family

---
+++ app/Controllers/Dataset_qc_ions.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Dataset_qc_ions extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "dataset_qc_ions";
+        $this->my_title = "Dataset QC Ions";
+    }
+}
+?>
