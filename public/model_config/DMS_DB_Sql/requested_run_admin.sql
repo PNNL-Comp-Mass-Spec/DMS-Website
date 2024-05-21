918e369c7436baf414b397b0d36db01976c0186e
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-20 21:14:07 -0700

    Remove InfoOnly parameter from procedure call

--- public/model_config/DMS_DB_Sql/requested_run_admin.sql
+++ public/model_config/DMS_DB_Sql/requested_run_admin.sql
@@ -52,7 +52,6 @@ INSERT INTO sproc_args VALUES(6,'NewWorkPackage','NewWorkPackage','varchar','inp
 INSERT INTO sproc_args VALUES(7,'RequestIdList','RequestIdList','varchar','input','2147483647','update_requested_run_wp');
 INSERT INTO sproc_args VALUES(8,'<local>','message','varchar','output','512','update_requested_run_wp');
 INSERT INTO sproc_args VALUES(9,'<local>','callingUser','varchar','input','128','update_requested_run_wp');
-INSERT INTO sproc_args VALUES(10,'InfoOnly','InfoOnly','tinyint','input','','update_requested_run_wp');
 INSERT INTO sproc_args VALUES(11,'<local>','mode','varchar','input','32','update_requested_run_assignments');
 INSERT INTO sproc_args VALUES(12,'param','newValue','varchar','input','512','update_requested_run_assignments');
 INSERT INTO sproc_args VALUES(13,'id','reqRunIDList','varchar','input','64000','update_requested_run_assignments');
