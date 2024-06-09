b987da36aee7e2e261bf30d303c38a8fd9d5c036
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-08 20:49:40 -0700

    Add page family for NEWT terms

---
+++ app/Controllers/Newt.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Newt extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "newt";
+        $this->my_title = "NEWT";
+    }
+}
+?>
