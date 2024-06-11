1791cffdab300c3f752f8d71ba1039811f06eadf
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-11 15:24:12 -0700

    Add Ontology CV page family

---
+++ app/Controllers/Ontology_cv.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Ontology_cv extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "ontology_cv";
+        $this->my_title = "Ontology CV";
+    }
+}
+?>
