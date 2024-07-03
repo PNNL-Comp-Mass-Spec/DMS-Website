143b93890f0844b243ca71eed7a891b387f9fdbc
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-02 20:42:42 -0700

    Add missing controller

---
+++ app/Controllers/Dataset_job_tracking.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Dataset_job_tracking extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "dataset_job_tracking";
+        $this->my_title = "Dataset Job Tracking";
+    }
+}
+?>
