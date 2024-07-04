1e67b194c585e6c868aff42dfb902fc72bc2b79d
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-04 15:08:12 -0700

    Sort activity report entries ascending

--- public/model_config/DMS_DB_Sql/dms_activity.sql
+++ public/model_config/DMS_DB_Sql/dms_activity.sql
@@ -2,5 +2,5 @@
 BEGIN TRANSACTION;
 CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO general_params VALUES('list_report_data_table','v_event_log_24_hour_summary');
-INSERT INTO general_params VALUES('list_report_data_sort_dir','DESC');
+INSERT INTO general_params VALUES('list_report_data_sort_dir','ASC');
 COMMIT;
