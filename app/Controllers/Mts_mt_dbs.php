4165f8400af4700eb9242dc390c5c80fb5cee8c7
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-14 22:07:40 -0700

    Update page family names

--- app/Controllers/Mts_mt_dbs.php
+++ app/Controllers/Mts_mt_dbs.php
@@ -5,7 +5,7 @@ class Mts_mt_dbs extends DmsBase {
     function __construct()
     {
         $this->my_tag = "mts_mt_dbs";
-        $this->my_title = "MTS AMT Tag";
+        $this->my_title = "MTS AMT Tag DBs";
     }
 }
 ?>
--- app/Controllers/Mts_pt_dbs.php
+++ app/Controllers/Mts_pt_dbs.php
@@ -5,7 +5,7 @@ class Mts_pt_dbs extends DmsBase {
     function __construct()
     {
         $this->my_tag = "mts_pt_dbs";
-        $this->my_title = "MTS Peptide";
+        $this->my_title = "MTS Peptide DBs";
     }
 }
 ?>
