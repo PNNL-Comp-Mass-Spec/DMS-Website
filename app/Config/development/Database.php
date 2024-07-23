30dde013942c8f83cc0977f0964ce714da7e4681
alchemistmatt <matthew.monroe@pnnl.gov>
2024-07-22 17:51:25 -0700

    Update comments in database connection settings files

--- app/Config/development/Database.php
+++ app/Config/development/Database.php
@@ -4,34 +4,34 @@
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
 | -------------------------------------------------------------------
-| This file will contain the settings needed to access your database.
+| This file will contain the settings needed to access your database
 |
 | For complete instructions please consult the 'Database Connection'
-| page of the User Guide.
+| page of the User Guide
 |
 | -------------------------------------------------------------------
 | EXPLANATION OF VARIABLES
 | -------------------------------------------------------------------
 |
-|   ['DSN']      The full DSN string describe a connection to the database.
-|   ['hostname'] The hostname of your database server.
+|   ['DSN']      The full DSN string describe a connection to the database
+|   ['hostname'] The hostname of your database server
 |   ['username'] The username used to connect to the database
 |   ['password'] The password used to connect to the database
 |   ['database'] The name of the database you want to connect to
-|   ['port']     The database port number. To use this value you have to add a line to the database config array.
-|   ['DBDriver'] The database driver. e.g.: MySQLi.
-|           Currently supported:
-|                MySQLi, Postgre, SQLite3, SQLSRV
+|   ['port']     The database port number; to use this value you have to add a line to the database config array
+|   ['DBDriver'] The database driver; e.g., MySQLi
+|                Currently supported:
+|                  MySQLi, Postgre, SQLite3, SQLSRV
 |   ['DBPrefix'] You can add an optional prefix, which will be added
-|                to the table name when using the  Query Builder class
+|                to the table name when using the Query Builder class
 |   ['pConnect'] true/false - Whether to use a persistent connection
-|   ['schema']   The database schema, default value varies by driver. Used by PostgreSQL and SQLSRV drivers.
-|   ['DBDebug']  true/false - Whether database errors should be displayed.
+|   ['schema']   The database schema, default value varies by driver; used by PostgreSQL and SQLSRV drivers
+|   ['DBDebug']  true/false - Whether database errors should be displayed
 |   ['charset']  The character set used in communicating with the database
 |   ['DBCollat'] The character collation used in communicating with the database
-|                NOTE: Only used in the ‘MySQLi’ driver.
+|                NOTE: Only used in the "MySQLi" driver
 |   ['swapPre']  A default table prefix that should be swapped with the DBPrefix
-|   ['encrypt']  Whether or not to use an encrypted connection.
+|   ['encrypt']  Whether to use an encrypted connection
 |
 |           'SQLSRV' and 'pdo/sqlsrv' drivers accept true/false
 |           'MySQLi' and 'pdo/mysql' drivers accept an array with the following options:
@@ -41,12 +41,12 @@
 |               'ssl_ca'     - Path to the certificate authority file
 |               'ssl_capath' - Path to a directory containing trusted CA certificats in PEM format
 |               'ssl_cipher' - List of *allowed* ciphers to be used for the encryption, separated by colons (':')
-|               'ssl_verify' - true/false; Whether verify the server certificate or not ('mysqli' only)
+|               'ssl_verify' - true/false; Whether verify the server certificate or not ('MySQLi' only)
 |
-|   ['compress'] Whether or not to use client compression (MySQL only)
+|   ['compress'] Whether to use client compression (MySQL only)
 |   ['strictOn'] true/false - forces 'Strict Mode' connections
 |                           - good for ensuring strict SQL while developing
-|   ['failover'] array - A array with 0 or more data for connections if the main should fail.
+|   ['failover'] array - A array with 0 or more data for connections to use if the main connection fails
 */
 
 /*
