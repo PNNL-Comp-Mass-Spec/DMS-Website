ce9d51a667a5bd0f0133621f2133257367496878
alchemistmatt <matthew.monroe@pnnl.gov>
2024-08-10 21:26:25 -0700

    Add PSM page families

---
+++ app/Controllers/Data_package_dataset_psm.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Data_package_dataset_psm extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "data_package_dataset_psm";
+        $this->my_title = "Data Package Dataset PSM Results";
+    }
+}
+?>
