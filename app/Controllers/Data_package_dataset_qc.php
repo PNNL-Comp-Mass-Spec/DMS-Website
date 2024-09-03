460b0ac6149177a460b19a01dea113b0e5e15b40
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-03 10:55:53 -0700

    Add Data Package Dataset QC page family

---
+++ app/Controllers/Data_package_dataset_qc.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Data_package_dataset_qc extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "data_package_dataset_qc";
+        $this->my_title = "Data Package Dataset QC";
+    }
+}
+?>
