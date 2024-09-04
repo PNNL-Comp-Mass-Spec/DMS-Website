4d302427f9e9b24ae475004b44652cc95604d2ce
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-04 15:32:18 -0700

    Add/update freezer hot links

--- public/model_config/DMS_DB_Sql/freezers.sql
+++ public/model_config/DMS_DB_Sql/freezers.sql
@@ -11,8 +11,10 @@ CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, 
 INSERT INTO list_report_primary_filter VALUES(1,'pf_Freezer','Freezer','32','','freezer','ContainsText','text','128','','');
 INSERT INTO list_report_primary_filter VALUES(2,'pf_Comment','Comment','32','','comment','ContainsText','text','128','','');
 CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
-INSERT INTO list_report_hotlinks VALUES(1,'containers','invoke_entity','freezer','material_location/report/-/~@','');
-INSERT INTO list_report_hotlinks VALUES(2,'freezer','invoke_entity','freezer','freezers/show/','');
+INSERT INTO list_report_hotlinks VALUES(1,'freezer','invoke_entity','freezer','freezers/show/','');
+INSERT INTO list_report_hotlinks VALUES(2,'freezer_tag','invoke_entity','freezer','material_location/report/-/~@','');
+INSERT INTO list_report_hotlinks VALUES(3,'containers','invoke_entity','freezer_tag','material_container/report/-/StartsWith__@','');
 CREATE TABLE detail_report_hotlinks ( idx INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Placement" text, "id" text, "options" text );
-INSERT INTO detail_report_hotlinks VALUES(1,'freezer','detail-report','freezer','material_location/report/-/~@','labelCol','dl_freezer','');
+INSERT INTO detail_report_hotlinks VALUES(1,'freezer','detail-report','freezer','material_location/report/-/~@','labelCol','dl_freezer_locations','');
+INSERT INTO detail_report_hotlinks VALUES(2,'containers','detail-report','freezer_tag','material_container/report/-/StartsWith__@','labelCol','dl_freezer_containers','');
 COMMIT;
