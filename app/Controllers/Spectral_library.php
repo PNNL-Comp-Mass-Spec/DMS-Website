cd00ca3e7898f0ae596459fe020030380b08c7df
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-18 17:13:15 -0700

    Add Spectral Library page family

---
+++ app/Controllers/Spectral_library.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Spectral_library extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "spectral_library";
+        $this->my_title = "Spectral Library";
+    }
+}
+?>
