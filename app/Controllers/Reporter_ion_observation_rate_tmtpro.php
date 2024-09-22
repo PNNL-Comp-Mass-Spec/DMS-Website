f507a8cd233ed4a00f5f5612da3759b9edb58492
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-22 16:17:59 -0700

    Add TMTpro reporter ion observation rate page family

---
+++ app/Controllers/Reporter_ion_observation_rate_tmtpro.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Reporter_ion_observation_rate_tmtpro extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "reporter_ion_observation_rate_tmtpro";
+        $this->my_title = "Reporter Ion Observation Rate TMTpro";
+    }
+}
+?>
