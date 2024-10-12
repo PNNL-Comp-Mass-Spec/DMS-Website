58b2e5212ad27412e042180e515f32193d9218b9
alchemistmatt <matthew.monroe@pnnl.gov>
2024-10-11 19:04:54 -0700

    Add new page family used to select multiple data packages

---
+++ app/Controllers/Helper_data_package_ckbx.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Helper_data_package_ckbx extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "helper_data_package_ckbx";
+        $this->my_title = "Data Package";
+    }
+}
+?>
