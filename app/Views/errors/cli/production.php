76c676b445aa0cf9da45b5c99367b78a220a7733
alchemistmatt <matthew.monroe@pnnl.gov>
2024-12-07 21:06:15 -0800

    Update CodeIgniter files

--- app/Views/errors/cli/production.php
+++ app/Views/errors/cli/production.php
@@ -1,5 +1,5 @@
 <?php
 
 // On the CLI, we still want errors in productions
-// So just use the exception template.
+// so just use the exception template.
 include __DIR__ . '/error_exception.php';
