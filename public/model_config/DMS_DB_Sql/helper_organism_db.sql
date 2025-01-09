340125d1e4ac84339f5fbbfdb465468475efb013
alchemistmatt <matthew.monroe@pnnl.gov>
2025-01-08 20:50:05 -0800

    Add tooltip on the is_decoy column

--- public/model_config/DMS_DB_Sql/helper_organism_db.sql
+++ public/model_config/DMS_DB_Sql/helper_organism_db.sql
@@ -4,7 +4,7 @@ CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO general_params VALUES('list_report_data_table','v_helper_organism_db_list_report');
 INSERT INTO general_params VALUES('list_report_data_sort_col','name');
 INSERT INTO general_params VALUES('list_report_data_sort_dir','ASC');
-INSERT INTO general_params VALUES('list_report_data_cols','name, organism, num_proteins, num_residues, id, created, size_mb, description');
+INSERT INTO general_params VALUES('list_report_data_cols','name, organism, num_proteins, num_residues, id, created, size_mb, description, is_decoy');
 INSERT INTO general_params VALUES('list_report_helper_multiple_selection','no');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO list_report_primary_filter VALUES(1,'pf_name','Name','50!','','name','ContainsText','text','128','','');
@@ -16,4 +16,5 @@ INSERT INTO list_report_hotlinks VALUES(3,'num_proteins','format_commas','value'
 INSERT INTO list_report_hotlinks VALUES(4,'num_residues','format_commas','value','','{"Decimals":"0"}');
 INSERT INTO list_report_hotlinks VALUES(5,'organism','invoke_entity','value','organism/report/~','');
 INSERT INTO list_report_hotlinks VALUES(6,'created','min_col_width','value','15','');
+INSERT INTO list_report_hotlinks VALUES(7,'is_decoy','column_tooltip','value','When &quot;Yes&quot;, the FASTA file has forward and reverse proteins','');
 COMMIT;
