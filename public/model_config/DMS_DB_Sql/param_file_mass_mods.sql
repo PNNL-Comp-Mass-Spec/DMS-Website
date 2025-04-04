9732c1921eda103286041ddd9c46895176afed6b
alchemistmatt <matthew.monroe@pnnl.gov>
2025-04-04 15:08:04 -0700

    Update list report hotlink to use an exact match

--- public/model_config/DMS_DB_Sql/param_file_mass_mods.sql
+++ public/model_config/DMS_DB_Sql/param_file_mass_mods.sql
@@ -15,7 +15,7 @@ INSERT INTO list_report_primary_filter VALUES(8,'pf_mass_max','Max Mass','','','
 INSERT INTO list_report_primary_filter VALUES(9,'pf_param_file_name','Param File','50!','','param_file_name','ContainsText','text','256','','');
 INSERT INTO list_report_primary_filter VALUES(10,'pf_primary_tool','Tool','','','primary_tool','ContainsText','text','','','');
 CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
-INSERT INTO list_report_hotlinks VALUES(1,'original_source_name','invoke_entity','value','unimod/report','');
+INSERT INTO list_report_hotlinks VALUES(1,'original_source_name','invoke_entity','value','unimod/report/~','');
 INSERT INTO list_report_hotlinks VALUES(2,'mass_correction_tag','invoke_entity','value','mass_correction_factors/report/-/~','');
 INSERT INTO list_report_hotlinks VALUES(3,'param_file_id','invoke_entity','value','param_file/show','');
 INSERT INTO list_report_hotlinks VALUES(4,'mod_id','invoke_entity','value','param_file_mass_mods/report/-/@','');
