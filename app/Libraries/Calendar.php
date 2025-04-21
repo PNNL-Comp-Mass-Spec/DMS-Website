47de2464c1e616c0237b1e3fe587f208b1672e52
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2025-04-21 15:16:58 -0700

    Move the single date_helper method into the class that has the single use of it

--- app/Libraries/Calendar.php
+++ app/Libraries/Calendar.php
@@ -447,8 +447,50 @@ class Calendar {
 	 */
 	public function get_total_days($month, $year)
 	{
-		helper('date');
-		return days_in_month($month, $year);
+		return $this->days_in_month($month, $year);
+	}
+
+	/**
+	 * Number of days in a month
+	 *
+	 * Takes a month/year as input and returns the number of days
+	 * for the given month/year. Takes leap years into consideration.
+	 *
+	 * @param	int	$month	a numeric month
+	 * @param	int|string	$year	a numeric year
+	 * @return	int
+	 */
+	function days_in_month(int $month, $year)
+	{
+		if ($month < 1 OR $month > 12)
+		{
+			return 0;
+		}
+		elseif ( ! is_numeric($year) OR strlen($year) !== 4)
+		{
+			$year = date('Y');
+		}
+
+		if (defined('CAL_GREGORIAN'))
+		{
+			return cal_days_in_month(CAL_GREGORIAN, $month, $year);
+		}
+
+		if ($year >= 1970)
+		{
+			return (int) date('t', mktime(12, 0, 0, $month, 1, $year));
+		}
+
+		if ($month == 2)
+		{
+			if ($year % 400 === 0 OR ($year % 4 === 0 && $year % 100 !== 0))
+			{
+				return 29;
+			}
+		}
+
+		$days_in_month	= array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+		return $days_in_month[$month - 1];
 	}
 
 	// --------------------------------------------------------------------
