4034d1c14e789184fbff6a3b5c45f62831ff6d75
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2025-08-04 17:26:45 -0700

    Update jQuery and jQuery UI

--- app/Views/resource_links/base2js.php
+++ app/Views/resource_links/base2js.php
@@ -1,7 +1,7 @@
 
-<script src="<?= base_url('javascript/jquery/jquery-3.6.0.js') ?>"></script>
-<script src="<?= base_url('javascript/jquery/jquery-migrate-3.3.2.js') ?>"></script>
-<script src="<?= base_url('javascript/jquery-ui-1.13.2/jquery-ui.min.js') ?>"></script>
+<script src="<?= base_url('javascript/jquery/jquery-3.7.1.min.js') ?>"></script>
+<script src="<?= base_url('javascript/jquery/jquery-migrate-3.5.0.min.js') ?>"></script>
+<script src="<?= base_url('javascript/jquery-ui-1.14.1/jquery-ui.min.js') ?>"></script>
 <script src="<?= base_url('javascript/jquery/jquery.event.drag-2.2.js') ?>"></script>
 <script src="<?= base_url('javascript/jquery/jquery.unobtrusive-ajax.min.js') ?>"></script>
 <script src="<?= base_url('javascript/spin.min.js?version=101') ?>"></script>
