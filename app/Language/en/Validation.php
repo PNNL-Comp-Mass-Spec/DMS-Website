da1af72d8ba4cd648308ee0118877347f1f35c5d
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2025-08-08 15:45:34 -0700

    Fix error message formatting for custom validation rules

--- app/Language/en/Validation.php
+++ app/Language/en/Validation.php
@@ -1,4 +1,9 @@
 <?php
 
 // override core en language system validation or define your own en language validation message
-return [];
+return [
+    'name_space'    => 'The {field} field must contain only letters, numbers, space, dash, or underscore',
+    'os_filename'   => 'The {field} field cannot contain any of these characters: \\ / : * ? . \" \' < > |',
+    'work_package'  => 'The {field} field must be a valid work package format',
+    'valid_date'    => 'Could not recognize ({value}) in {field} field as a valid date',
+];
--- app/Validation/DmsValidationRules.php
+++ app/Validation/DmsValidationRules.php
@@ -7,6 +7,15 @@ namespace App\Validation;
 class DmsValidationRules
 {
     /**
+     * NOTE: See app/Language/en/Validation.php for error strings for 2-parameter rules
+     * Also see https://codeigniter4.github.io/userguide/libraries/validation.html#setting-custom-error-messages
+     * and https://codeigniter4.github.io/userguide/libraries/validation.html#creating-custom-rules
+     * for information on custom rules and the options used in error strings
+     * IMPORTANT: to use the {field}, {param}, and {value} tags in error messages, the error message must be 
+     * set in app/Language/en/Validation.php, not by setting the value of $error.
+     */
+
+    /**
      * Dummy method to allow use in DmsValidationPreformat without validation errors
      * @param string $str
      * @return bool
@@ -18,58 +27,58 @@ class DmsValidationRules
         return true;
     }
 
-    function char_set($str, string $fields, array $data, string &$error = null) : bool
+    function char_set($str, string $params, array $data, ?string &$error = null, string $field = '') : bool
     {
-        $charSet = explode(',', $fields)[0];
+        $charSet = explode(',', $params)[0];
         $result = (preg_match("/^[$charSet]+$/", $str)) ? true : false;
         if (!$result)
         {
-            $error = "The %s field must contain only characters from '$charSet'";
+            $error = "The $field field must contain only characters from '$charSet'";
         }
         return $result;
     }
 
-    function char_set_base($str, string $fields, array $data, string &$error = null) : bool
+    function char_set_base($str, string $params, array $data, ?string &$error = null, string $field = '') : bool
     {
-        $charSet = explode(',', $fields)[0];
+        $charSet = explode(',', $params)[0];
         $pattern = "/^([" . $charSet . "])+$/";
         $result = (!preg_match("$pattern", $str)) ? false : true;
         if (!$result)
         {
-            $error = "The %s field must contain only characters from '$charSet'";
+            $error = "The $field field must contain only characters from '$charSet'";
         }
         return $result;
     }
 
-    function name_space($str, string &$error = null) : bool
+    function name_space($str, ?string &$error = null) : bool
     {
         $result = (!preg_match("/^([ a-zA-Z0-9_-])+$/", $str)) ? false : true;
-        if (!$result)
-        {
-            $error = "The %s field must contain only letters, numbers, space, dash, or underscore";
-        }
+        //if (!$result)
+        //{
+        //    $error = "The {field} field must contain only letters, numbers, space, dash, or underscore";
+        //}
         return $result;
     }
 
-    function os_filename($str, string &$error = null) : bool
+    function os_filename($str, ?string &$error = null) : bool
     {
         // Note that we do not allow periods to prevent folder names from containing periods and to prevent filenames from containing a period before the file extension
         $result = (!preg_match("/^([ a-zA-Z0-9_!@#$%^&(){}\[\];,-])+$/", $str)) ? false : true;
-        if (!$result)
-        {
-            $error = "The %s field cannot contain any of these characters: \\ / : * ? . \" ' < > |";
-        }
+        //if (!$result)
+        //{
+        //    $error = "The {field} field cannot contain any of these characters: \\ / : * ? . \" ' < > |";
+        //}
         return $result;
     }
 
-    function work_package($str, string &$error = null) : bool
+    function work_package($str, ?string &$error = null) : bool
     {
         $parm = '[A-Za-z][A-Za-z0-9]{5}';
         $result = (preg_match("/$parm/", $str)) ? true : false;
-        if (!$result)
-        {
-            $error = "The %s field must be a valid work package format";
-        }
+        //if (!$result)
+        //{
+        //    $error = "The {field} field must be a valid work package format";
+        //}
         return $result;
     }
 
@@ -84,15 +93,15 @@ class DmsValidationRules
         return true;
     }
 
-    function not_contain($str, string $fields, array $data, string &$error = null) : bool
+    function not_contain($str, string $params, array $data, ?string &$error = null, string $field = '') : bool
     {
-        $fields = explode(',', $fields);
+        $params = explode(',', $params);
 
-        foreach ($fields as $field)
+        foreach ($params as $param)
         {
-            if (strpos($str, $field) !== false)
+            if (strpos($str, $param) !== false)
             {
-                $error = "The %s field can not contain the word '$field'";
+                $error = "The $field field can not contain the word '$param'";
                 return false;
             }
         }
@@ -100,12 +109,12 @@ class DmsValidationRules
         return true;
     }
 
-    function valid_date($str, string &$error = null) : bool
+    function valid_date($str, ?string &$error = null) : bool
     {
         $t = strtotime($str);
         if (!$t)
         {
-            $error = "Could not recognize '$str' in %s field as a valid date";
+            //$error = "Could not recognize '$str' in {field} field as a valid date"; // "Could not recognize '{value}' in {field} field as a valid date"
             return false;
         }
         else
