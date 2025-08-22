6502c8bae3c6680c328c73595bc9c012afcdad27
alchemistmatt <matthew.monroe@pnnl.gov>
2025-08-21 19:42:11 -0700

    Rename page family

--- app/Controllers/Dataset_svc_center_report_state.php
+++ app/Controllers/Dataset_svc_center_report_state.php
@@ -1,11 +1,11 @@
 <?php
 namespace App\Controllers;
 
-class Dataset_cc_report_state extends DmsBase {
+class Dataset_svc_center_report_state extends DmsBase {
     function __construct()
     {
-        $this->my_tag = "dataset_cc_report_state";
-        $this->my_title = "Dataset Cost Center Report State";
+        $this->my_tag = "dataset_svc_center_report_state";
+        $this->my_title = "Dataset Service Center Report State";
     }
 }
 ?>
