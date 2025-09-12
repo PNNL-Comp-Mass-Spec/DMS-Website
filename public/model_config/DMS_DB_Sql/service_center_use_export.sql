a9844e706a61b02447980193bf955aa7f1845aa6
alchemistmatt <matthew.monroe@pnnl.gov>
2025-09-11 19:21:41 -0700

    Show ticket number on the service center use export page instead of entry_id

--- public/model_config/DMS_DB_Sql/service_center_use_export.sql
+++ public/model_config/DMS_DB_Sql/service_center_use_export.sql
@@ -2,12 +2,12 @@
 BEGIN TRANSACTION;
 CREATE TABLE general_params ( "name" text, "value" text );
 INSERT INTO general_params VALUES('list_report_data_table','v_service_center_use_export');
-INSERT INTO general_params VALUES('list_report_data_sort_col','report_id, entry_id');
+INSERT INTO general_params VALUES('list_report_data_sort_col','report_id, "TicketNumber"');
 INSERT INTO general_params VALUES('list_report_data_sort_dir','DESC');
 CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
 INSERT INTO list_report_hotlinks VALUES(1,'report_id','invoke_entity','value','service_center_use_report/report/','');
-INSERT INTO list_report_hotlinks VALUES(2,'entry_id','invoke_entity','value','service_center_use/show/','');
+INSERT INTO list_report_hotlinks VALUES(2,'TicketNumber','invoke_entity','value','service_center_use/show/','');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO list_report_primary_filter VALUES(1,'pf_report_id','Report ID','','','report_id','Equals','text','','','');
-INSERT INTO list_report_primary_filter VALUES(2,'pf_entry_id','Entry ID','','','entry_id','Equals','text','','','');
+INSERT INTO list_report_primary_filter VALUES(2,'pf_ticket_number','Ticket Number (Entry_ID)','','','"TicketNumber"','StartsWithText','text','','','');
 COMMIT;
