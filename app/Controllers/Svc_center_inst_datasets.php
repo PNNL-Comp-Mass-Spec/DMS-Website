56132054f94a4d8adc0059f4022b90fd7f91d590
alchemistmatt <matthew.monroe@pnnl.gov>
2025-09-27 19:37:51 -0700

    Add service center instrument datasets page family

---
+++ app/Controllers/Svc_center_inst_datasets.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controllers;
+
+class Svc_center_inst_datasets extends DmsBase {
+    function __construct()
+    {
+        $this->my_tag = "svc_center_inst_datasets";
+        $this->my_title = "Service Center Instrument Datasets";
+    }
+}
+?>
