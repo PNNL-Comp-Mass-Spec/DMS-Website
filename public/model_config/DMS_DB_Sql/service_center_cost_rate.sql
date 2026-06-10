287d373be83f50998cf4c34011e63755fd37a5e0
alchemistmatt <matthew.monroe@pnnl.gov>
2026-06-09 18:13:32 -0700

    Update hotlinks

--- public/model_config/DMS_DB_Sql/service_center_cost_rate.sql
+++ public/model_config/DMS_DB_Sql/service_center_cost_rate.sql
@@ -6,8 +6,8 @@ INSERT INTO general_params VALUES('list_report_data_sort_col','cost_group_id, se
 INSERT INTO general_params VALUES('list_report_data_sort_dir','ASC');
 CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
 INSERT INTO list_report_hotlinks VALUES(1,'cost_group_id','invoke_entity','value','service_center_use_report/report/-/-/@','');
-INSERT INTO list_report_hotlinks VALUES(2,'service_type_id','invoke_entity','value','service_center_use_type/report','');
-INSERT INTO list_report_hotlinks VALUES(3,'doe_burdened_rate','invoke_entity','cost_group_id','service_center_cost_rate_burdened/report/','');
+INSERT INTO list_report_hotlinks VALUES(2,'service_type_id','invoke_entity','no_value','service_center_use_type/report','');
+INSERT INTO list_report_hotlinks VALUES(3,'doe_rate','invoke_entity','cost_group_id','service_center_cost_rate_burdened/report/','');
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO list_report_primary_filter VALUES(1,'pf_cost_group_id','Cost Group ID','7!','','cost_group_id','Equals','text','','','');
 INSERT INTO list_report_primary_filter VALUES(2,'pf_service_type','Service Type','30!','','service_type','ContainsText','text','','','');
