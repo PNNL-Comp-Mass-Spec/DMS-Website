3c0b2e208f12fbec260939c3624b85d20c878c5d
alchemistmatt <matthew.monroe@pnnl.gov>
2026-07-21 18:12:37 -0700

    Add primary filters to helper pages

--- public/model_config/DMS_DB_Sql/helper_requested_run.sql
+++ public/model_config/DMS_DB_Sql/helper_requested_run.sql
@@ -9,8 +9,9 @@ INSERT INTO general_params VALUES('list_report_data_cols','request AS Sel, reque
 CREATE TABLE list_report_primary_filter ( id INTEGER PRIMARY KEY,  "name" text, "label" text, "size" text, "value" text, "col" text, "cmp" text, "type" text, "maxlength" text, "rows" text, "cols" text );
 INSERT INTO list_report_primary_filter VALUES(1,'pf_name','Name','35!','','name','ContainsText','text','128','','');
 INSERT INTO list_report_primary_filter VALUES(2,'pf_experiment','Experiment','25!','','experiment','ContainsTextTPO','text','128','','');
-INSERT INTO list_report_primary_filter VALUES(3,'pf_instrument','Instrument','32','','instrument','ContainsText','text','128','','');
-INSERT INTO list_report_primary_filter VALUES(4,'pf_requester','Requester','32','','requester','ContainsText','text','128','','');
+INSERT INTO list_report_primary_filter VALUES(3,'pf_batch','Batch','15','','batch','Equals','text','64','','');
+INSERT INTO list_report_primary_filter VALUES(4,'pf_instrument','Instrument','32','','instrument','ContainsText','text','128','','');
+INSERT INTO list_report_primary_filter VALUES(5,'pf_requester','Requester','32','','requester','ContainsText','text','128','','');
 CREATE TABLE list_report_hotlinks ( id INTEGER PRIMARY KEY,  "name" text, "LinkType" text, "WhichArg" text, "Target" text, "Options" text );
 INSERT INTO list_report_hotlinks VALUES(1,'sel','CHECKBOX','request','','');
 INSERT INTO list_report_hotlinks VALUES(2,'request','update_opener','value','','');
